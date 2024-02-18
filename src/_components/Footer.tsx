13674d52f763b59dac52072b779e3448dee90b47
JoaoMaciel5G <juauvitu765@gmail.com>
2024-02-18 15:24:08 -0300

    feat: add component footer

---
+++ src/_components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <div className="w-full bg-secondary px-5 py-6">
+            <p className="text-gray-400 text-sm font-bold opacity-75">&copy; 2023 Copyright BarberShop</p>
+        </div>
+    );
+}
+ 
+export default Footer;
